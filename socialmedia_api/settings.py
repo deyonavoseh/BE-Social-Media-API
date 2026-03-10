71a99caaac550b78956ca6be5a65a7739e7c0534
Deyon Avoseh <avodeyon.07@gmail.com>
2026-03-10 23:07:38 +0100

    fix: add render domain to allowed hosts

--- socialmedia_api/settings.py
+++ socialmedia_api/settings.py
@@ -5,8 +5,9 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+ALLOWED_HOSTS += ['be-social-media-api.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
